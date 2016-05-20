81541a2342d7dfbd52cae819862ab8c56438a6b6
RoyShefi <roy.shefi@gmail.com>
2016-05-21 01:47:13 +0300

    final main - prep for mis 8.

--- src/arkanoid/game/Ass5Game.java
+++ src/arkanoid/game/Ass5Game.java
@@ -28,11 +28,29 @@ public class Ass5Game {
         KeyboardSensor ks = gui.getKeyboardSensor();
         ArrayList<LevelInformation> levels = new ArrayList<>();
         // Adding levels class to the array.
-        levels.add(new Level1());
-        levels.add(new Level2());
-        levels.add(new Level3());
-        levels.add(new Level4());
+
+        if (args.length == 0) {
+            levels.add(new Level1());
+            levels.add(new Level2());
+            levels.add(new Level3());
+            levels.add(new Level4());
+        }
+
         // Creating a game.
+        for (int i =0; i < args.length; i++) {
+            if (args[i].equals("1")){
+                levels.add(new Level1());
+            }
+            if (args[i].equals("2")){
+                levels.add(new Level2());
+            }
+            if (args[i].equals("3")){
+                levels.add(new Level3());
+            }
+            if (args[i].equals("4")){
+                levels.add(new Level4());
+            }
+        }
         GameFlow g = new GameFlow(ar, ks, gui, new Counter(), new Counter());
         g.runLevels(levels);
     }
